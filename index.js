0e556c6c57464eae3ab6223629e7ad89fd0d06e0
Christian Ortiz <c.ortiz313@gmail.com>
2023-09-19 13:31:15 -0400

    Moved routes to a router
    Moved all book routes to booksRouter so it can handle all requests for a book. Can do the same for every data model, or separate by feature in the future.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5000;
 const mongoDBURL = process.env.MONGODB_URL;
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
+import booksRouter from "./routes/booksRouter.js";
 
 const app = express();
 
@@ -19,118 +20,9 @@ app.get("/", (req, res) => {
   return res.status(234).send("Hello World");
 });
 
-// Route to create a book
-app.post("/books", async (req, res) => {
-  try {
-    if (
-      !req.body.title ||
-      !req.body.author ||
-      !req.body.genre ||
-      !req.body.copiesSold ||
-      !req.body.rating ||
-      !req.body.ISBN ||
-      !req.body.publisher ||
-      !req.body.price ||
-      !req.body.description ||
-      !req.body.yearPublished
-    ) {
-      return res.status(400).send("Missing required fields");
-    }
-
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      genre: req.body.genre,
-      copiesSold: req.body.copiesSold,
-      rating: req.body.rating,
-      ISBN: req.body.ISBN,
-      publisher: req.body.publisher,
-      price: req.body.price,
-      description: req.body.description,
-      yearPublished: req.body.yearPublished,
-    };
-    const book = await Book.create(newBook);
-    return res.status(201).send(book);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send(`Internal Server Error ${error.message}`);
-  }
-});
-
-// Route to get all books
-app.get("/books", async (req, res) => {
-  try {
-    const books = await Book.find({});
-    return res.status(200).json({
-      count: books.length,
-      data: books,
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send(`Internal Server Error ${error.message}`);
-  }
-});
-
-// Route to get one book BY ISBN
-app.get("/books/isbn/:isbn", async (req, res) => {
-  try {
-    const isbn = parseInt(req.params.isbn);
-    const book = await Book.find({ ISBN: isbn });
-    return res.status(200).json(book);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send(`Internal Server Error ${error.message}`);
-  }
-});
-
-// Route to update a book by ISBN
-app.put("/books/isbn/:isbn", async (req, res) => {
-  try {
-    if (
-      !req.body.title ||
-      !req.body.author ||
-      !req.body.genre ||
-      !req.body.copiesSold ||
-      !req.body.rating ||
-      !req.body.ISBN ||
-      !req.body.publisher ||
-      !req.body.price ||
-      !req.body.description ||
-      !req.body.yearPublished
-    ) {
-      return res.status(400).send("Missing required fields");
-    }
-    const isbn = parseInt(req.params.isbn);
-    const result = await Book.findOneAndUpdate({ ISBN: isbn }, req.body);
-
-    if (!result) {
-      return res.status(404).json({ message: "Book not found" });
-    }
-    return res.status(200).send({ message: "Book updated successfully" });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send(`Internal Server Error ${error.message}`);
-  }
-});
-
-// Route to delete a book by ISBN
-app.delete("/books/isbn/:isbn", async (req, res) => {
-  try {
-    const isbn = parseInt(req.params.isbn);
-
-    const result = await Book.findOneAndDelete({ ISBN: isbn });
-
-    if (!result) {
-      return res.status(404).json({ message: "Book not found" });
-    }
-
-    return res.status(200).send({ message: "Book deleted successfully" });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send(`Internal Server Error ${error.message}`);
-  }
-});
 /////////////////////////////////////////////////////////////
+// Router to handle books
+app.use("/books", booksRouter);
 
 mongoose
   .connect(mongoDBURL)
---
+++ routes/booksRouter.js
@@ -0,0 +1,118 @@
+import express from "express";
+import { Book } from "../models/bookModel.js";
+
+const router = express.Router();
+
+// Route to create a book
+router.post("/", async (req, res) => {
+  try {
+    if (
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.genre ||
+      !req.body.copiesSold ||
+      !req.body.rating ||
+      !req.body.ISBN ||
+      !req.body.publisher ||
+      !req.body.price ||
+      !req.body.description ||
+      !req.body.yearPublished
+    ) {
+      return res.status(400).send("Missing required fields");
+    }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      copiesSold: req.body.copiesSold,
+      rating: req.body.rating,
+      ISBN: req.body.ISBN,
+      publisher: req.body.publisher,
+      price: req.body.price,
+      description: req.body.description,
+      yearPublished: req.body.yearPublished,
+    };
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(`Internal Server Error ${error.message}`);
+  }
+});
+
+// Route to get all books
+router.get("/", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(`Internal Server Error ${error.message}`);
+  }
+});
+
+// Route to get one book BY ISBN
+router.get("/isbn/:isbn", async (req, res) => {
+  try {
+    const isbn = parseInt(req.params.isbn);
+    const book = await Book.find({ ISBN: isbn });
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(`Internal Server Error ${error.message}`);
+  }
+});
+
+// Route to update a book by ISBN
+router.put("/isbn/:isbn", async (req, res) => {
+  try {
+    if (
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.genre ||
+      !req.body.copiesSold ||
+      !req.body.rating ||
+      !req.body.ISBN ||
+      !req.body.publisher ||
+      !req.body.price ||
+      !req.body.description ||
+      !req.body.yearPublished
+    ) {
+      return res.status(400).send("Missing required fields");
+    }
+    const isbn = parseInt(req.params.isbn);
+    const result = await Book.findOneAndUpdate({ ISBN: isbn }, req.body);
+
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.status(200).send({ message: "Book updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(`Internal Server Error ${error.message}`);
+  }
+});
+
+// Route to delete a book by ISBN
+router.delete("/isbn/:isbn", async (req, res) => {
+  try {
+    const isbn = parseInt(req.params.isbn);
+
+    const result = await Book.findOneAndDelete({ ISBN: isbn });
+
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    return res.status(200).send({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(`Internal Server Error ${error.message}`);
+  }
+});
+
+export default router;
