53b67a763aebb2b718b34a793f02d8358b410f89
dnimo02 <112497768+dnimo02@users.noreply.github.com>
2023-09-21 22:30:07 -0500

    Fix ShoppingCartController class
    ShoppingCartController.js had the wrong class name. Switched it to its correct name.
    
    Added Feature comment
    
    Added Sprint feature comment.

--- controllers/ShoppingCartController.js
+++ controllers/ShoppingCartController.js
@@ -3,6 +3,9 @@ import express from "express";
 import { User } from "./models/userModel.js";
 import { Book } from "../models/bookModel.js";
 
-class ProfileManagementController {}
+class ShoppingCartController {
+    // 3.1 Retrieve the subtotal price of all items in the user’s shopping cart.
 
-export default ProfileManagementController;
+}
+
+export default ShoppingCartController;
