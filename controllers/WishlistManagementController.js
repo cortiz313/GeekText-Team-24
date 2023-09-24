5600858322ae28136da4a9116c7a39288629df72
Christian Osorio <chrisosorio@Christians-MacBook-Pro.local>
2023-09-24 16:53:20 -0400

    Began work on Wishlist Controller
    Began working on the wishlist controller, however couldnt get it to create the wishlist through postman

--- controllers/WishlistManagementController.js
+++ controllers/WishlistManagementController.js
@@ -3,7 +3,36 @@ import express from "express";
 import { User } from "./models/userModel.js";
 import { Book } from "../models/bookModel.js";
 import { Wishlist } from "../models/wishlistModel.js";
+import error from "mongoose/lib/error/index.js";
+
+const WishlistController = express.Router();
+//Feature 6.1: Create Wishlist for a user with unique name
+
+WishlistController.post('/createWishlist', async (req, res) => {
+    const createWishlist = {
+        "name" : String,
+        "username" : String
+    }
+    try{
+        if(!req.body.name){
+            return res.status(400).send("Missing field: name ");
+        }
+        if(!req.body.username){
+            return res.status(400).send("User does not exist ");
+        }
+
+        const wishlist = await Wishlist.create(newWishlist);
+        return res.status(201).send(wishlist);
+
+    }catch (error){
+        console.log(error.message);
+        res.status(500).send(error.message);
+    }
+   
+
+});
 
 class BookBrowsingController {}
 
-export default BookBrowsingController;
+
+export default WishlistController;
