7d6b340a628376a70526e512d394f933cc03df7a
dnimo02 <112497768+dnimo02@users.noreply.github.com>
2023-09-25 21:30:10 -0400

    Add features to Shopping cart
    Updated user model to add userID, and updated shopping cart to take bookSchema instead of String.

--- controllers/ShoppingCartController.js
+++ controllers/ShoppingCartController.js
@@ -5,7 +5,44 @@ import { Book } from "../models/bookModel.js";
 
 class ShoppingCartController {
     // 3.1 Retrieve the subtotal price of all items in the user’s shopping cart.
+    async getSubtotalPrice(req, res) {
+        try { 
+            const user = await User.find({ userID: req.params.userID });
+            const shoppingCart = user.shoppingCart;
+            let total = 0;
 
+            for (let book of shoppingCart) {
+                total += book.price;
+            }
+
+            return res.status(200).json({
+                shoppingCartTotal: total,
+              });
+        } catch (error) {
+            console.log(error.message);
+        res.status(500).send(`Internal Server Error ${error.message}`);
+        }
+    }
+    // 3.2 Add a book to the shopping cart.
+    async addBookToShoppingCart(req, res) {
+        try {
+            
+        } catch (error) {
+
+        }
+    }
+    // 3.3 Retrieve the list of books added to cart.
+    async getBookList(req, res) {
+      try {
+        const user = await User.find({ userID: req.params.userID });
+        return res.status(200).json({
+          shoppingCart: user.shoppingCart,
+        });
+      } catch (error) {
+        console.log(error.message);
+        res.status(500).send(`Internal Server Error ${error.message}`);
+      }
+    }
 }
 
 export default ShoppingCartController;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 const PORT = process.env.PORT;
-const mongoDBURL = "mongodb+srv://justinr:dUj4ckiX26ARCyG4@geektext-team-24.zgqn2ve.mongodb.net/?retryWrites=true&w=majority";
+const mongoDBURL = process.env.mongoDB_URL;
 import mongoose from "mongoose";
 import booksRouter from "./routes/booksRouter.js";
 import BookDetailsController from "./controllers/BookDetailsController.js";
