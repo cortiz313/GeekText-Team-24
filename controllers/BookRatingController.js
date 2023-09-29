86e8ceb2045d70c8dad003d2422a05e16d599a5e
Christian Ortiz <c.ortiz313@gmail.com>
2023-09-29 12:32:06 -0400

    Updated Book Rating for User
    Uncommented User

--- controllers/BookRatingController.js
+++ controllers/BookRatingController.js
@@ -5,51 +5,46 @@ import { Rating } from "../models/ratingModel.js";
 
 const BookRatingController = express.Router();
 
-BookRatingController.post('/', async (req, res) => 
-{
-    try 
-    {
-        if(!req.body.username)
-        {
-            return res.status(400).send("Missing field: username ");
-        }
-        if(!req.body.rating)
-        {
-            return res.status(400).send("Missing field: rating ");
-        }
-        if(!req.body.ISBN)
-        {
-            return res.status(400).send("Missing field: ISBN ");
-        }
-
-        const isbn = req.body.ISBN;
-        const username = req.body.username;
-        const rating = req.body.rating;
-
-        const book = await Book.findOne({ISBN:isbn});
-        if(!book) {
-            return res.status(404).json({message:`Book not found!`});
-        }
-
-        /*const user = await User.findOne({username:username});
-        if(!user) {
-            return res.status(404).json({message:`User not found!`});
-        }*/
-
-        const newRating = new Rating({
-            username:username,
-            rating:rating,
-            ISBN:isbn,
-        });
-
-        const createdRating = await Rating.create(newRating);
-        return res.status(201).send({message:"Rating created successfully!", rating:createdRating});
+BookRatingController.post("/", async (req, res) => {
+  try {
+    if (!req.body.username) {
+      return res.status(400).send("Missing field: username ");
     }
-    catch(error)
-    {
-        console.log(error.message);
-        res.status(500).send(error.message);
+    if (!req.body.rating) {
+      return res.status(400).send("Missing field: rating ");
     }
+    if (!req.body.ISBN) {
+      return res.status(400).send("Missing field: ISBN ");
+    }
+
+    const isbn = req.body.ISBN;
+    const username = req.body.username;
+    const rating = req.body.rating;
+
+    const book = await Book.findOne({ ISBN: isbn });
+    if (!book) {
+      return res.status(404).json({ message: `Book not found!` });
+    }
+
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      return res.status(404).json({ message: `User not found!` });
+    }
+
+    const newRating = new Rating({
+      username: username,
+      rating: rating,
+      ISBN: isbn,
+    });
+
+    const createdRating = await Rating.create(newRating);
+    return res
+      .status(201)
+      .send({ message: "Rating created successfully!", rating: createdRating });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(error.message);
+  }
 });
 
 export default BookRatingController;
