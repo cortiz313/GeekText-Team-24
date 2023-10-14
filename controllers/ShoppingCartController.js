f7144a6f8fa13a8d1e206febda9fa061a7314d53
dnimo02 <112497768+dnimo02@users.noreply.github.com>
2023-10-13 19:37:45 -0500

    Add the rest of the features for Daniela
    Add features 3.1, 3.3, 3.4 to shopping cart controller.

--- controllers/ShoppingCartController.js
+++ controllers/ShoppingCartController.js
@@ -5,34 +5,107 @@ import { Book } from "../models/bookModel.js";
 
 const ShoppingCartController = express.Router();
 
-  // 3.2 Add a book to the shopping cart.
-  ShoppingCartController.post('/', async (req, res) => {
-    try {
-      
-      const bookId = req.body.bookId;
-      const userId = req.body.userId;
-      
-      // Find user by userID
-      const user = await User.findOne({ username: userId });
-      if(!user) {
-        return res.status(404).json({message: "User not found"});
-      }
+// 3.1 Retrieve the subtotal price of all items in the user’s shopping cart.
+ShoppingCartController.get("/subtotal", async (req, res) => {
+  try {
+    const userId = req.body.userId;
 
-      // Find book by bookID
-      const book = await Book.findOne({ ISBN: bookId });
-      if(!book) {
-        return res.status(404).json({message: "Book not found"});
-      }
-
-      // Add book to shopping cart
-      await user.shoppingCart.push(book);
-      await user.save();
-
-      return res.status(200).json({ message: "Book added to shopping cart"});
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).send(`Internal Server Error ${error.message}`);
+    // Find user
+    const user = await User.findOne({ username: userId });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  });
+
+    // Get subtotal by adding price of each book
+    let subtotal = 0;
+    user.shoppingCart.forEach((item) => {
+      subtotal += item.price;
+    });
+
+    return res.status(200).json({ message: `Your subtotal is: $${subtotal.toFixed(2)}` });
+    } catch (error) {
+    console.log(error.message);
+    res.status(500).send(`Internal Server Error ${error.message}`);
+  }
+});
+
+// 3.2 Add a book to the shopping cart.
+ShoppingCartController.post("/addToCart", async (req, res) => {
+  try {
+    const bookId = req.body.bookId;
+    const userId = req.body.userId;
+
+    // Find user by userID
+    const user = await User.findOne({ username: userId });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Find book by bookID
+    const book = await Book.findOne({ ISBN: bookId });
+    if (!book) {
+      return res.status(404).json({ message: "Book not found in shopping cart" });
+    }
+
+    // Add book to shopping cart
+    await user.shoppingCart.push(book);
+    await user.save();
+
+    return res.status(200).json({ message: "Book added to shopping cart" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(`Internal Server Error ${error.message}`);
+  }
+});
+
+// 3.3 Retrieve the list of book(s) in the user’s shopping cart.
+ShoppingCartController.get("/list", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+
+    // Find user by userID
+    const user = await User.findOne({ username: userId });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    let shoppingList = user.shoppingCart
+
+    return res.status(200).json({ message: shoppingList });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(`Internal Server Error ${error.message}`);
+  }
+});
+
+// 3.4 Remove a book from shopping cart. 
+ShoppingCartController.delete("/delete", async (req, res) => {
+  try {
+    const bookId = req.body.bookId;
+    const userId = req.body.userId;
+
+    // Find user by userID
+    const user = await User.findOne({ username: userId });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Find book by bookID
+    const book = user.shoppingCart.find((item) => item.ISBN == bookId);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found in shopping cart" });
+    }
+
+    // Delete book from cart
+    const bookIndex = user.shoppingCart.indexOf(book);
+    user.shoppingCart.splice(bookIndex, 1);
+    await user.save();
+
+    return res.status(200).json({ message: "Book deleted from shopping cart" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(`Internal Server Error ${error.message}`);
+  }
+});
 
 export default ShoppingCartController;
