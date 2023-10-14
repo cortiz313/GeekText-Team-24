36733098912ce710e17bd74c6f141a086bd89f53
Chris Osorio <chrisosorio1011@gmail.com>
2023-10-13 20:01:41 -0400

    Adding book to user wishlist
    Added feature to add books to user's existing wishlist

--- controllers/WishlistManagementController.js
+++ controllers/WishlistManagementController.js
@@ -34,14 +34,15 @@ WishlistController.post('/:username', async (req, res) => {
     
     const username = req.params.username;
 
-    const result = await User.findOneAndUpdate(
+    const userExists = await User.findOneAndUpdate(
       { username: username },
       { $push: { "wishlist": { name: req.body.name, items: [] } } },
     );
 
-    if (!result) {
+     if (!userExists) {
       return res.status(404).json({ message: "User not found" });
     }
+  
 
     return res.status(200).send({ message: "User's wishlist updated successfully" });
   } catch (error) {
@@ -49,5 +50,57 @@ WishlistController.post('/:username', async (req, res) => {
     res.status(500).send(error.message);
   }
 });
+
+WishlistController.post('/:username/:wishlistName/addbook', async (req, res) => {
+  try {
+    if (!req.body.bookId){
+      return res.status(400).send("Missing bookID");
+    }
+    const usernameParam = req.params.username;
+    const wishlistParam = req.params.wishlistName;
+    const bookId = req.body.bookId;
+
+    // Find the user
+    const userExist = await User.findOne({ username: usernameParam });
+    if (!userExist) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    // Find the user's Wishlist
+    const wishlistExist = userExist.wishlist.find(wish => wish.name === wishlistParam);
+    if (!wishlistExist) {
+      return res.status(404).json({ message: "Wishlist not found for the user" });
+    }
+
+    // Check if bookID exists in DB
+    if (!mongoose.Types.ObjectId.isValid(bookId)) {
+      return res.status(400).json({ message: "Invalid book ID" });
+    }
+
+    // Check if the bookId is already in the wishlist
+    if (wishlistExist.items.some(item => item.bookId.toString() === bookId)) {
+      return res.status(400).json({ message: "Book is already in the wishlist" });
+    }
+
+    // If bookId is not in the wishlist, add it
+    const updatedUser = await User.findOneAndUpdate(
+      { username: usernameParam, 'wishlist.name': wishlistParam },
+      { $push: { 'wishlist.$.items': { bookId: bookId } } },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "Failed to update User's Wishlist" });
+    }
+
+    return res.status(200).json({ message: "Book added to " +usernameParam + "'s wishlist successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(error.message);
+  }
+});
+
+
+
+
 
 export default WishlistController;
