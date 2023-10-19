8a6decf9a2edf78231ad5e7d83e34277afe87f90
Chris Osorio <chrisosorio1011@gmail.com>
2023-10-18 20:56:30 -0400

    Remove bookID from user wishlist

--- controllers/WishlistManagementController.js
+++ controllers/WishlistManagementController.js
@@ -109,4 +109,64 @@ WishlistController.post('/:username/:wishlistName/addbook', async (req, res) => 
     res.status(500).send(error.message);
   }
 });
+
+//Feature 6.3 Remove book from user wishlist and add to shopping cart.
+WishlistController.delete('/:username/:wishlistName/addToShoppingCart', async (req, res) => {
+  try{
+    if (!req.body.bookId){
+      return res.status(400).send("Missing bookID");
+    }
+    const userParam = req.params.username
+    const wishNameParam = req.params.wishlistName
+    const bookIdBody = req.body.bookId
+
+    //Find User
+    const userExists = await User.findOne({username: userParam});
+
+    if(!userExists){
+      return res.status(400).json({message: "User does not exist"})
+    }
+
+    //Find User Wishlist
+    const wishlistExists = await userExists.wishlist.find(wish => wish.name === wishNameParam);
+    
+    if(!wishlistExists){
+      return res.status(400).json({message: "User does not have a wishlist under that name."})
+    }
+
+    //Determine if book exists in database
+    if (!mongoose.Types.ObjectId.isValid(bookIdBody)) {
+      return res.status(400).json({ message: "Invalid book ID" });
+    } 
+
+    // Check if the bookId is not in the wishlist
+    if (!wishlistExists.items.some(item => item.bookId.toString() === bookIdBody)) {
+      return res.status(400).json({ message: "Book is not in the wishlist" });
+    }
+
+    // TODO: If bookId is in the wishlist, remove it and add to shopping cart
+    const updatedWishlist = await User.findOneAndUpdate(
+      { username: userParam, 'wishlist.name': wishNameParam },
+      { $pull: { 'wishlist.$.items': { bookId: bookIdBody } } },
+      { new: true }
+    );
+
+    if (!updatedWishlist) {
+      return res.status(404).json({ message: "Failed to update User" });
+    }
+
+    //TODO: change method of searching book, instead of object ID, need to use book ISBN
+    //TODO: change wishlist schema for items: Book: to use isbn instead of objectID
+
+    await userExists.shoppingCart.push(bookIdBody);
+    await userExists.save();
+    return res.status(200).json({ message: "Book added to shopping cart" });
+
+    
+  }catch (error) {
+    console.log(error.message);
+    res.status(500).send(error.message);
+  }
+});
+
 export default WishlistController;
