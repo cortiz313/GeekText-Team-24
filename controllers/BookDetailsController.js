57458092301263fd6be7e6aec791d258fe91a46b
Christian Ortiz <c.ortiz313@gmail.com>
2023-10-27 14:39:13 -0400

    Fixed comments, ratings, author issues
    Fixed so that when creating a book, it checks for existing author, made it so comments can be created from create book, instead of direct duplication we create comment object and use reference, same with rating.

--- controllers/BookDetailsController.js
+++ controllers/BookDetailsController.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import express from "express";
 import { Book } from "../models/bookModel.js";
 import { Author } from "../models/authorModel.js";
+import { Rating } from "../models/ratingModel.js";
+import { Comment } from "../models/commentModel.js";
 
 const BookDetailsController = express.Router();
 
@@ -50,20 +52,56 @@ BookDetailsController.post("/createBook", async (req, res) => {
       });
     }
 
-    const newBook = new Book({
+    const existingAuthor = await Author.findOne({ firstName, lastName });
+    let authorId = null;
+    if (!existingAuthor) {
+      // Option 1: Create the author if they do not exist
+      const newAuthor = new Author(author);
+      await newAuthor.save();
+      authorId = newAuthor._id;
+    } else {
+      // Option 2: Use the existing author's ID
+      authorId = existingAuthor._id;
+    }
+
+    // Create comments if they are provided
+    let commentIds = [];
+    if (req.body.comments && Array.isArray(req.body.comments)) {
+      const comments = req.body.comments.map((comment) => ({
+        ...comment,
+        ISBN: req.body.ISBN,
+      }));
+      const createdComments = await Comment.insertMany(comments);
+      commentIds = createdComments.map((comment) => comment._id);
+    }
+
+    // Create ratings if they are provided
+    let ratingIds = [];
+    if (req.body.ratings && Array.isArray(req.body.ratings)) {
+      const ratings = req.body.ratings.map((rating) => ({
+        ...rating,
+        ISBN: req.body.ISBN,
+      }));
+      const createdRatings = await Rating.insertMany(ratings);
+      ratingIds = createdRatings.map((rating) => rating._id);
+    }
+
+    const bookData = {
       title: req.body.title,
-      author: author,
+      author: authorId,
       genre: req.body.genre,
       copiesSold: req.body.copiesSold,
-      rating: req.body.rating,
       ISBN: req.body.ISBN,
       publisher: req.body.publisher,
       price: req.body.price,
       description: req.body.description,
       yearPublished: req.body.yearPublished,
-    });
+      ratings: ratingIds, // Include ratings
+      comments: commentIds, // Include comments
+    };
 
-    const book = await Book.create(newBook);
+    const newBook = new Book(bookData);
+    const book = await newBook.save();
 
     // Add the book to the author's booksWritten array
     author.booksWritten.push(book._id);
@@ -80,7 +118,10 @@ BookDetailsController.post("/createBook", async (req, res) => {
 BookDetailsController.get("/isbn/:isbn", async (req, res) => {
   try {
     const isbn = parseInt(req.params.isbn);
-    const book = await Book.find({ ISBN: isbn });
+    const book = await Book.find({ ISBN: isbn })
+      .populate("ratings")
+      .populate("author")
+      .populate("comments");
     return res.status(200).json(book);
   } catch (error) {
     console.log(error.message);
@@ -89,43 +130,46 @@ BookDetailsController.get("/isbn/:isbn", async (req, res) => {
 });
 
 // Feature 4.3: Create an author given that Author's details
-BookDetailsController.post('/createAuthor', async (req, res) => 
-{
-    try 
-    {
-        if (!req.body.firstName)
-        {
-            return res.status(400).send("Missing field: firstName ");
-        }
-        if (!req.body.lastName)
-        {
-            return res.status(400).send("Missing field: lastName ");
-        }
-        if (!req.body.biography)
-        {
-            return res.status(400).send("Missing field: biography ");
-        }
-        if (!req.body.publisher)
-        {
-            return res.status(400).send("Missing field: publisher ");
-        }
-
-        const newAuthor = 
-        {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            biography: req.body.biography,
-            publisher: req.body.publisher,
-        };
-
-        const author = await Author.create(newAuthor);
-        return res.status(201).send(author);
+BookDetailsController.post("/createAuthor", async (req, res) => {
+  try {
+    if (!req.body.firstName) {
+      return res.status(400).send("Missing field: firstName ");
     }
-    catch (error)
-    {
-        console.log(error.message);
-        res.status(500).send(error.message);
+    if (!req.body.lastName) {
+      return res.status(400).send("Missing field: lastName ");
     }
-}); 
+    if (!req.body.biography) {
+      return res.status(400).send("Missing field: biography ");
+    }
+    if (!req.body.publisher) {
+      return res.status(400).send("Missing field: publisher ");
+    }
+
+    const { firstName, lastName, biography, publisher } = req.body;
+
+    // Check if the author already exists
+    const existingAuthor = await Author.findOne({ firstName, lastName });
+    if (existingAuthor) {
+      return res.status(409).json({
+        message: "Author already exists",
+        author: existingAuthor,
+      });
+    }
+
+    // If the author does not exist, create a new one
+    const newAuthorData = {
+      firstName,
+      lastName,
+      biography,
+      publisher,
+    };
+
+    const newAuthor = await Author.create(newAuthorData);
+    return res.status(201).json(newAuthor);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error: " + error.message);
+  }
+});
 
 export default BookDetailsController;
